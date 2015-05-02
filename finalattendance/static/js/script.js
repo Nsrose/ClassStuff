8f778778daeec66de77490797699164a2776692e
nsrose <nsrose@berkeley.edu>
2015-05-02 15:05:57 -0700

    more work

--- finalattendance/static/js/script.js
+++ finalattendance/static/js/script.js
@@ -4,29 +4,57 @@ var FIREBASE_STRING = "https://finalattendance.firebaseio.com/";
 
 // Custom alert box
 function message(m) {
-    $("#overlay").show();
-    
-    
+    $("#overlay").fadeIn(400);
+    $("#message_container").fadeIn(400);
+    $("#message_text").text(m);
 }
 
 // Close message box
 function closeMessage() {
-
+    $("#message_container").fadeOut(400);
+    $("#overlay").fadeOut(400);
+    hideYesNo();
 }
 
 // Show yes no options on message box. 
-function showYesNo() {
+function showYesNo(login, name) {
+    $("#info_container").show();
+    $("#name").text(name);
+    $("#login").text(login);
+    $("#yesno_container").show();
+}
 
+// Hides the yesno container
+function hideYesNo() {
+    $("#yesno_container").hide();
+    $("#info_container").hide();
 }
 
 // Checks if name and login inputs are alright
 function inputsOkay(name, login) {
+    if (name == "" || login == "") {
+        return false;
+    }
     return true;
 }
 
 $(document).ready(function() {
     // Create a connection to firebase:
     var fireRef = new Firebase(FIREBASE_STRING);
+
+    // Do submit upon pressing enter
+    $('#login_input').keypress(function (e) {
+        if (e.which == 13) {
+            $("#submit").click();
+            return false;
+        }
+    });
+    $('#name_input').keypress(function (e) {
+        if (e.which == 13) {
+            $("#submit").click();
+            return false;
+        }
+    });
 
     // Form submit actions
     $("#submit").click(function() {
@@ -36,25 +64,38 @@ $(document).ready(function() {
             fireRef.once('value', function(snapshot) {
                 if (snapshot.hasChild(login)) {
                     message("We already got this login's attendance.");
-                    closeMessage();
+                    
                 } else {
-                    verifyAndRecord(login, name);
+                    verify(login, name);
                 }
             });
         } else {
             message("Name or login is bad.");
-            closeMessage();
+            window.setTimeout(function() {
+                closeMessage();    
+            }, 1000)
         }
         
     })
 
     // Verifies that user wants to send data and sends if good.
-    function verifyAndRecord(login, name) {
+    function verify(login, name) {
         message("Are you sure you want to record attendance? You can only do this once.");
-        showYesNo();
-        fireRef.child(login).set(name);
-        message("Thanks for recording your attendance!");
-        closeMessage();
+        showYesNo(login, name);
+        $("#yes").click(function() {
+            closeMessage();
+            fireRef.child(login).set(name);
+            window.setTimeout(function() {
+                message("Thanks for recording your attendance!");
+                window.setTimeout(function() {
+                    closeMessage();
+                }, 3000)
+            }, 1000)
+        })
+        $("#no").click(function() {
+            closeMessage();
+        })
     }
+
 
 })
