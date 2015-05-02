e23587ebd87c75f579157e3c9f7dfbe5ba6a3991
nsrose <nsrose@berkeley.edu>
2015-05-01 19:49:33 -0700

    working

--- finalattendance/static/js/script.js
+++ finalattendance/static/js/script.js
@@ -2,6 +2,23 @@
 var FIREBASE_STRING = "https://finalattendance.firebaseio.com/";
 
 
+// Custom alert box
+function message(m) {
+    $("#overlay").show();
+    
+    
+}
+
+// Close message box
+function closeMessage() {
+
+}
+
+// Show yes no options on message box. 
+function showYesNo() {
+
+}
+
 // Checks if name and login inputs are alright
 function inputsOkay(name, login) {
     return true;
@@ -18,18 +35,26 @@ $(document).ready(function() {
         if (inputsOkay(name, login)) {
             fireRef.once('value', function(snapshot) {
                 if (snapshot.hasChild(login)) {
-                    alert("We already got this login's attendance.");
+                    message("We already got this login's attendance.");
+                    closeMessage();
                 } else {
-                    var node = {};
-                    node[login] = name;
-                    fireRef.push(node);
-                    alert("Thanks for recording your attendance!");
+                    verifyAndRecord(login, name);
                 }
             });
         } else {
-            alert("Name or login is bad.");
+            message("Name or login is bad.");
+            closeMessage();
         }
         
     })
+
+    // Verifies that user wants to send data and sends if good.
+    function verifyAndRecord(login, name) {
+        message("Are you sure you want to record attendance? You can only do this once.");
+        showYesNo();
+        fireRef.child(login).set(name);
+        message("Thanks for recording your attendance!");
+        closeMessage();
+    }
 
 })
